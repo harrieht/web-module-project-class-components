fa5aadf79c186ec7ea998e84e462fa1730103e60
Mary Gabon <marygabon21@gmail.com>
2023-08-20 23:13:23 -0700

    update

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,13 +1,27 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  state = {
+    name: '',
+  }
+
+  onSumbit = evt => {
+    
+  }
+
+  onChange = evt => {
+    const { value } =evt.target
+    this.setState({
+      name: value
+    })
+  }
   render() {
-    const {addTodo} = this.props
+    const { addTodo } = this.props
     return (
-      <div>
-        Form
-        <button onClick={evt => addTodo('Go to Concert')}>add todo</button>
-      </div>
+      <form onClick={this.onSumbit}>
+        <input type="text" value={this.state.name} onChange={this.onChange} />
+        <input type="submit" />
+      </form>
     )
   }
 }
